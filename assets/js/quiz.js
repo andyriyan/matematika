fd662768e447f8bc3b0c42eda764a0b8416aeec3
andyriyan <69646005+andyriyan@users.noreply.github.com>
2026-05-09 21:02:42 +0700

    Update quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -13,11 +13,10 @@ const QuizManager = {
   
   async init() {
     try {
-      // Periksa protokol
-      if (window.location.protocol === 'file:') {
-        alert("Aplikasi harus dijalankan menggunakan Local Server untuk memuat file quiz.json.");
-        return;
-      }
+    // Memastikan aplikasi tidak dijalankan langsung dari double-click file .html
+    if (window.location.protocol === 'file:') {
+      console.warn("Menjalankan via file:// akan memblokir fitur fetch di beberapa browser.");
+    }
       
       // Ambil slug materi dari URL parameter (misal: ?materi=statistika-smp)
       const urlParams = new URLSearchParams(window.location.search);
